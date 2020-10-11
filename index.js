4a1e4ac8826baaefe8bcd7b04617c9d207a60456
adeadu555 <ade.adu555@gmail.com>
2020-10-10 23:08:35 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 var inquirer = require('inquirer');
+var fs = require('fs');
+
 inquirer
   .prompt([
+      {
+          type: "input",
+          name: "title",
+          message: "What is the title of your project?"
+      },
+
+      {
+        type: "input",
+        name: "description",
+        message: "Describe your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the installation process?"
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use your application?"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Which is the license?"
+        options: ["MIT", "NoLicense"],
+
+    },
+
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the other contributors?"
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "How is your application tested?"
+    },
+
     /* Pass your questions in here */
   ])
   .then(answers => {
